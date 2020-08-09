83d8cbdfe75603efa1ff19b4d262a428975a8b9c
dnhirapara <darshikhirapara@gmail.com>
2020-08-10 01:01:41 +0530

    JS files addes

---
+++ assets/Student/JS/assign.js
@@ -0,0 +1,11 @@
+var rows = document.getElementById("row");
+var row = rows[0];
+var assign_table = document.getElementById("assign-table");
+assign_table.insertRow(0);
+
+// for (i = 0; i < 5; i++) {
+//     var assign_row = assign_table.insertRow(1);
+//     for (j = 0; j < 5; j++) {
+//         assign_row.insertCell(j).innerHTML = "hello";
+//     }
+// }
