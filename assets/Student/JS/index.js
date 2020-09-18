c7a8a85331b4f35c71d2b32ea0dbc7e76d0309d3
dnhirapara <darshikhirapara@gmail.com>
2020-09-18 21:04:46 +0530

    bug fixes

--- assets/Student/JS/index.js
+++ assets/Student/JS/index.js
@@ -13,19 +13,29 @@
 //         __click = 0;
 //     }
 // }
+
+// REF: https://www.w3schools.com/jquery/event_toggle.asp#:~:text=The%20toggle()%20method%20attaches,function%20fires%2C%20and%20so%20on.
+// REF: https://www.tutorialrepublic.com/faq/how-to-create-jquery-slide-left-and-right-toggle-effect.php
+
+
 $(document).ready(function () {
     $("#id_closeBtn").click(function () {
-        $('#idsideBar').toggle("slow", function () {
-            console.log($('#id_closeBtn').attr('class'));
-            if ($('#id_closeBtn').hasClass('fa-times')) {
-                $('#id_closeBtn').removeClass('fa-times');
-                $('#id_closeBtn').addClass('fa-bars');
-            } else {
-                $('#id_closeBtn').removeClass('fa-bars');
-                $('#id_closeBtn').addClass('fa-times');
-            }
-        });
+        $("#idsideBar").animate({
+            width: "toggle"
+        }, 1000);
     });
+    // $("#id_closeBtn").click(function () {
+    //     $('#idsideBar').toggle("slow", function () {
+    //         console.log($('#id_closeBtn').attr('class'));
+    //         if ($('#id_closeBtn').hasClass('fa-times')) {
+    //             $('#id_closeBtn').removeClass('fa-times');
+    //             $('#id_closeBtn').addClass('fa-bars');
+    //         } else {
+    //             $('#id_closeBtn').removeClass('fa-bars');
+    //             $('#id_closeBtn').addClass('fa-times');
+    //         }
+    //     });
+    // });
     for (var i = 0; i < 50; i++) {
         $("#id_subTable").find('tbody')
             .append($('<tr>')
@@ -44,3 +54,31 @@ $(document).ready(function () {
             );
     }
 });
+
+/* chartjs Attendence Chart */
+var ctx = document.getElementById('attChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Absent', 'Present', 'Leave'],
+        datasets: [{
+            label: 'Attendece',
+            data: [6, 19, 2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        duration: 50000,
+    }
+});
+/* chartjs Attendence Chart */
